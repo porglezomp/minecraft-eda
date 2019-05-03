ebe09b534c514518b5b095f4c6486b708269667d
C Jones <code@calebjones.net>
2019-05-03 12:31:51 -0700

    Update benchmark to match the yosys 4-bit counter
    The other minecraft logic synthesis projects have used this as their
    placement benchmark, so it's good to match them.

--- example/counter.sv
+++ example/counter.sv
@@ -1,15 +1,13 @@
-module counter #(parameter WIDTH=3, parameter MAX=(1<<WIDTH)-1) (
+module counter #(parameter WIDTH=4) (
     input clock,
     input reset,
+    input enable,
     output logic [WIDTH-1:0] count,
-    output logic max
 );
-
-assign max = count == MAX;
 
 always_ff @(posedge clock) begin
     if (reset) count <= 0;
-    else if (!max) count <= count + 1;
+    else if (enable) count <= count + 1;
 end
 
 endmodule
